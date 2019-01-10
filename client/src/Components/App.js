ba81b54fe5c7dfeb3e399af3b009c55dab2d7eee
Tony Chen <tc56007n@pace.edu>
2019-01-10 14:59:24 -0500

    first commit

---
+++ client/src/Components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Greetings from './Greetings/Greetings';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={Greetings} />
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        )
+    }
+}
+
+export default App;
